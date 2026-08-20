00baee2b3f6757ff7de5e6e374cc89712c3cbdf2
Wynn Pham <phamkimquybh@gmail.com>
2026-08-19 22:28:07 -0700

    Add: valid-sudoku - submission-4

---
+++ Data Structures & Algorithms/valid-sudoku/submission-4.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        unordered_set<char> rows[9];
+        unordered_set<char> cols[9];
+        unordered_set<char> boxes[9];
+
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+                if (board[r][c] == '.') {
+                    continue;
+                }
+
+                char val = board[r][c];
+                int box_idx = (r / 3) * 3 + (c / 3);
+
+                if (rows[r].count(val) || cols[c].count(val) || boxes[box_idx].count(val)) {
+                    return false;
+                }
+
+                rows[r].insert(val);
+                cols[c].insert(val);
+                boxes[box_idx].insert(val);
+            }
+        }
+        return true;
+    }
+};
